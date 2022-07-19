f74d765673e4ea603d3f9b4bdc10f9808b1ef2ef
Lamya iguidre <lamss93@hotmail.fr>
2022-07-19 19:48:37 +0200

    projet corrigé

--- front/html/cart.js
+++ front/html/cart.js
@@ -267,8 +267,8 @@ function validation(contact) {
   fetch("http://localhost:3000/api/products/order", {
     method: "POST",
     headers: {
-      Accept: "application / json",
-      "Content-Type": "application / json",
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(dataToSend),
   })
